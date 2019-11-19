8370062eb2466da4a51e1925b5f7a2de6cdc8af2
Andy Soell <andy@teamsoell.com>
2019-11-19 09:03:42 +0100

    move license creation code to an event

--- app/Http/Controllers/LicenseController.php
+++ app/Http/Controllers/LicenseController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\License;
 use Illuminate\Http\Request;
-use GuzzleHttp\Client as GuzzleClient;
 
 class LicenseController extends Controller
 {
@@ -13,55 +12,11 @@ class LicenseController extends Controller
      *
      * @return void
      */
-    public function checkin(String $machine_id, Request $request, GuzzleClient $guzzle)
+    public function checkin(String $machine_id, Request $request)
     {
         $license = License::firstOrCreate([
             'machine_id' => $machine_id,
         ]);
-
-        if ($license->wasRecentlyCreated &&
-            $endpoint = config('services.slack.webhook_endpoint')) {
-            $fields = [
-                [
-                    'type' => 'mrkdwn',
-                    'text' => sprintf("*IP:*\n%s", $request->ip()),
-                ],
-                [
-                    'type' => 'mrkdwn',
-                    'text' => sprintf("*Machine ID:*\n%s", $machine_id),
-                ],
-            ];
-
-            try {
-                if (($ipstack_api_key = config('services.ipstack.api_key')) &&
-                    ($response = (new $guzzle())->request('POST', sprintf('http://api.ipstack.com/%s?access_key=%s', $request->ip(), $ipstack_api_key))) &&
-                    ($location = json_decode($response->getBody()->getContents()))) {
-                    $fields[] = [
-                        'type' => 'mrkdwn',
-                        'text' => sprintf("*Location:*\n%s", $location->city ?? $location->country ?? 'unknown'),
-                    ];
-                }
-
-                (new $guzzle())->request('POST', $endpoint, [
-                    'body' => json_encode([
-                        'blocks' => [
-                            [
-                                'type' => 'section',
-                                'text' => [
-                                    'type' => 'mrkdwn',
-                                    'text' => 'New license issued',
-                                ],
-                            ],
-                            [
-                                'type'   => 'section',
-                                'fields' => $fields,
-                            ],
-                        ],
-                    ]),
-                ]);
-            } catch (\Exception $exception) {
-            }
-        }
 
         if ( ! $license->is_valid) {
             return response(null, 403);
--- app/License.php
+++ app/License.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use GuzzleHttp\Client as GuzzleClient;
 
 class License extends Model
 {
@@ -13,6 +14,56 @@ class License extends Model
     protected $primaryKey = 'machine_id';
     protected $keyType = 'string';
     public $incrementing = false;
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::created(function (License $license) {
+            if ($endpoint = config('services.slack.webhook_endpoint')) {
+                $fields = [
+                    [
+                        'type' => 'mrkdwn',
+                        'text' => sprintf("*IP:*\n%s", app('Illuminate\Http\Request')->ip()),
+                    ],
+                    [
+                        'type' => 'mrkdwn',
+                        'text' => sprintf("*Machine ID:*\n%s", $license->machine_id),
+                    ],
+                ];
+
+                try {
+                    if (($ipstack_api_key = config('services.ipstack.api_key')) &&
+                        ($response = (new GuzzleClient())->request('POST', sprintf('http://api.ipstack.com/%s?access_key=%s', app('Illuminate\Http\Request')->ip(), $ipstack_api_key))) &&
+                        ($location = json_decode($response->getBody()->getContents()))) {
+                        $fields[] = [
+                            'type' => 'mrkdwn',
+                            'text' => sprintf("*Location:*\n%s", $location->city ?? $location->country ?? 'unknown'),
+                        ];
+                    }
+
+                    (new GuzzleClient())->request('POST', $endpoint, [
+                        'body' => json_encode([
+                            'blocks' => [
+                                [
+                                    'type' => 'section',
+                                    'text' => [
+                                        'type' => 'mrkdwn',
+                                        'text' => 'New license issued',
+                                    ],
+                                ],
+                                [
+                                    'type'   => 'section',
+                                    'fields' => $fields,
+                                ],
+                            ],
+                        ]),
+                    ]);
+                } catch (\Exception $exception) {
+                }
+            }
+        });
+    }
 
     public function checkins()
     {
